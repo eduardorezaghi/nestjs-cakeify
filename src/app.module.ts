2d15004253fccc5bf58d10f931c219906e992caa
Eduardo Rezaghi <58532229+eduardorezaghi@users.noreply.github.com>
2023-03-21 23:11:53 -0300

    Add CakesController to app.module.ts for testing

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CakesController } from './cakes.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, CakesController],
   providers: [AppService],
 })
 export class AppModule {}
