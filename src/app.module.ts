53088c85b3b1c988178f51f4b1becc6658a067e5
Eduardo Rezaghi <58532229+eduardorezaghi@users.noreply.github.com>
2023-03-27 00:17:15 -0300

    Add cakes to app.module file

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CakesController } from './cakes.controller';
+import { CakesService } from './cakes/cakes.service';
 
 @Module({
   imports: [],
   controllers: [AppController, CakesController],
-  providers: [AppService],
+  providers: [AppService, CakesService],
 })
 export class AppModule {}
