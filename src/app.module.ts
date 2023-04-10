fe3e4aea4bc671d29e7d4832f744015adba3176e
Eduardo Rezaghi <58532229+eduardorezaghi@users.noreply.github.com>
2023-04-09 23:12:32 -0300

    Add TypeORM and pg dependencies for Postgres database integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,27 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CakesController } from './cakes.controller';
 import { CakesService } from './cakes/cakes.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CakesModule } from './cakes/cakes.module';
+
 
 @Module({
-  imports: [],
+  imports: [CakesModule,
+    TypeOrmModule.forRoot({
+        type: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        username: 'postgres',
+        password: 'pass123',
+        database: 'postgres',
+        autoLoadEntities: true,
+        // Disable ```synchronize``` flag on production!
+        synchronize: true,
+    }),
+  ],
   controllers: [AppController, CakesController],
   providers: [AppService, CakesService],
 })
