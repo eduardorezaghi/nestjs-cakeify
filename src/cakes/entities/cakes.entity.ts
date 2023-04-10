0e3e9d4b2da412b351754acaf7bd087e19b02fe0
Eduardo Rezaghi <58532229+eduardorezaghi@users.noreply.github.com>
2023-04-09 23:20:36 -0300

    Add TypeORM decorators for example entity

--- src/cakes/entities/cakes.entity.ts
+++ src/cakes/entities/cakes.entity.ts
@@ -1,12 +1,24 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
 /**
  * Class for model rules
  * for creating a `cake` object.
  */
 
-export interface Cakes {
+@Entity()
+export class Cakes {
+    @PrimaryGeneratedColumn()
     id: number;
+
+    @Column()
     flavour: string;
+
+    @Column()
     makingDate: Date;
+
+    @Column()
     expirationDate: Date;
+
+    @Column('json', { nullable: true })
     ingredients: Object;
 }
