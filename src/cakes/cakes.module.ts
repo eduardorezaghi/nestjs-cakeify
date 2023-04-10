27821b332b0bffad90e6ff65cba547c7caee47e5
Eduardo Rezaghi <58532229+eduardorezaghi@users.noreply.github.com>
2023-04-09 23:21:38 -0300

    Add TypeORM imports on example app module.ts

--- src/cakes/cakes.module.ts
+++ src/cakes/cakes.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CakesController } from 'src/cakes.controller';
 import { CakesService } from './cakes.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Cakes } from './entities/cakes.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Cakes])],
   controllers: [CakesController],
   providers: [CakesService],
 })
