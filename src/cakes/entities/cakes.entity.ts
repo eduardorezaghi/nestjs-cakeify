3bd9302d2e9f21e4f8df1d591a9cd6632516c623
Eduardo Rezaghi <58532229+eduardorezaghi@users.noreply.github.com>
2023-04-09 21:44:10 -0300

    Add global validation pipe example

--- src/cakes/entities/cakes.entity.ts
+++ src/cakes/entities/cakes.entity.ts
@@ -1,12 +1,12 @@
 /**
-* Class for model rules
-* for creating a `cake` object.
-*/
+ * Class for model rules
+ * for creating a `cake` object.
+ */
 
 export interface Cakes {
-    id: number,
-    flavour: string,
-    makingDate: Date,
-    expirationDate: Date,
-    ingredients: Object
+    id: number;
+    flavour: string;
+    makingDate: Date;
+    expirationDate: Date;
+    ingredients: Object;
 }
